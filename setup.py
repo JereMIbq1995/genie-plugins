bd76a32b48fca1b294b6a93842549d791dc4e4d8
Jeremy Duong <jeremibq@gmail.com>
2021-10-09 18:38:13 -0600

    added __init__.py to make constants a package/ changed package version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="UTF-8") as f:
 
 setuptools.setup(
     name="genie-plugins-jeremi1995",
-    version="0.0.1",
+    version="0.0.2",
     author="Jeremy Duong",
     author_email="jeremibq@gmail.com",
     description="genie-pluggins",
