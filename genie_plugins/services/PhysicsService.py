4a5602bc2a88ddc1fe516ed0050869e93c99ecea
jeremi1995 <jeremibq@gmail.com>
2021-10-13 10:46:12 -0600

    added a few methods for physics service

--- genie_plugins/services/PhysicsService.py
+++ genie_plugins/services/PhysicsService.py
@@ -7,4 +7,24 @@ class PhysicsService:
         pass
 
     def check_collision(self, actor1, actor2):
+        """
+            - get Body
+            - create pygame.Shape
+            - call colliderect
+        """
+        pass
+
+    def is_above(self, actor1, actor2):
+        """
+            Return true if actor1 is above actor2, and false otherwise
+        """
+        pass
+
+    def is_below(self, actor1, actor2):
+        pass
+
+    def is_left_of(self, actor1, actor2):
+        pass
+
+    def is_right_of(self, actor1, actor2):
         pass
